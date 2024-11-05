f89a55f68e5beb9f6d6ff82e6efc08c428fe35a1
muzammil <raajadobemania@gmail.com>
2024-11-05 10:32:27 +0500

    Initial Commit

---
+++ PdfViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PdfViewer
+//
+//  Created by r a a j on 05/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PdfViewer/PdfViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfViewerApp.swift
+//  PdfViewer
+//
+//  Created by r a a j on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PdfViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
